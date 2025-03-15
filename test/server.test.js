06988eeba3daa0e6648990b4199fac3f29a81701
Marcelo Manoel <marcelo_l_filho@ufms.br>
2025-03-15 12:09:18 -0400

    feat: initialize server using express and starting test

---
+++ test/server.test.js
@@ -0,0 +1,12 @@
+const supertest = require("supertest")
+const app = require("../src/app")
+const request = supertest(app)
+
+test("A aplicação deve responder na porta 3131", () => {
+    return request.get("/").then( res => {
+        let status = res.statusCode
+        expect(status).toEqual(200)
+    }).catch(err => {
+        fail(err)
+    })
+})
